9616c3a906a2ea954fb8196c9370295ff5f7c287
Sascha Böck <wi16b142@technikum-wien.at>
2017-12-08 22:41:32 +0100

    edit xaml

--- CodingDojo6/CodingDojo6/ViewModel/MainViewModel.cs
+++ CodingDojo6/CodingDojo6/ViewModel/MainViewModel.cs
@@ -37,13 +37,38 @@ namespace CodingDojo6.ViewModel
 
             Items = new ObservableCollection<ItemsVM>();
             Wishlist = new ObservableCollection<ItemsVM>();
-            Items.Add(new ItemsVM("Lego", "-", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
-            Items.Add(new ItemsVM("Playmobil", "-", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            DummyData();
 
             buyBtnClicked = new RelayCommand(() =>
            {
                Wishlist.Add(Selection);
            });
+        }
+
+        private void DummyData()
+        {
+            Items.Add(new ItemsVM("Lego", "-", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items.Add(new ItemsVM("Playmobil", "-", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+
+            Items[0].ItemsAdd(new ItemsVM("Lego 1", "5+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 2", "4+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 3", "3+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 4", "6+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 5", "7+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 6", "8+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 7", "9+", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 8", "10", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[0].ItemsAdd(new ItemsVM("Lego 9", "11", new BitmapImage(new Uri("Images/lego.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 1", "2", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 2", "3", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 3", "5", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 4", "6+", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 5", "15", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 6", "6", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 7", "6", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 8", "3", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+            Items[1].ItemsAdd(new ItemsVM("Playmobil 9", "1", new BitmapImage(new Uri("Images/playmobil.jpg", UriKind.Relative))));
+
         }
     }
 }
